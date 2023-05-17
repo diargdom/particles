2a1b182f3c99569666a1cb3750bfda4e45d07b0e
Diego Argumedo <diegoargumedo.jcol@orange.com>
2023-05-17 11:08:48 -0500

    FEAT: eje Y para imagenes among-us

--- src/components/Particle.jsx
+++ src/components/Particle.jsx
@@ -658,7 +658,7 @@ export default function Particle() {
                   },
                   "position": {
                       "x": -5,
-                      "y": 55
+                      "y": 50
                   }
               }
           }}
